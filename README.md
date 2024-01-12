f41c2c36ab5d45e72ab2e0d4363e1aa0c36b5ea8
Gastondnc <gastondnc@icloud.com>
2024-01-12 17:34:56 +0100

    chore: adding basic functionality and styles

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 2)-Obtener dos cartas del mazo adquirido.
 3)-Una vez obtenidas las cartas deberemos pasar los values de string a número.
 4)-Pintar las carta que nos trae dividíendolas en jugador y máquina.
+
+# Lógica del Juego
+1)- A quien corresponde la carta más alta
+2)- El ganador de la mano se queda con hambas cartas
+3)- Se reinicia la mano
+4)- El juego termina cuando no hay mas cartas disponibles en el mazo
+5)- Y el ganador en el que tengas más cartas en su mazo
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,58 +9,73 @@ import { Card, CardsResponse, Deck } from './models/deck.model';
 })
 export class AppComponent {
   public title = 'Card Game';
+  public isLoading: boolean = false;
+  public isDraw: boolean = false;
+  public isShowModal: boolean = false;
   public newDeckUrl: string = NEW_DECK;
   public newCardsUrl: string = NEW_CARDS;
   public newDeck: Partial<Deck> = {};
-  public newCards: Card[] = [];
-
+  public handCards: Card[] = [];
+  public cpu: Card[] = [];
+  public player: Card[] = [];
+  public remaining: number = 0;
+  public winner = '';
+  public endGameMessage: string = '';
 
   constructor() {
-    this.getNewDeck(this.newDeckUrl);
-
+    window.addEventListener('keyup', (event) => {
+      if (event.key === 'Escape' && this.isShowModal === true) {
+        this.closeModal()
+      }
+    })
   }
 
   // Llamamos al Endpoint que nos trae el mazo de cartas //
-  getNewDeck(url: string) {
-    fetch(url)
+  getNewDeck() {
+    fetch(this.newDeckUrl)
       .then(resp => resp.json())
       .then((data: Deck) => {
-        console.log(data)
+        // console.log(data)
         this.newDeck = data
-        this.getNewCards(this.newCardsUrl)
+        this.getHandCards()
       })
   }
 
   // Llamada al EndPoint que nos trae las cartas //
-  getNewCards(urlCards: string) {
-    console.log('NEWDECK', this.newDeck);
-    urlCards = urlCards.replace('<<deck_id>>', this.newDeck.deck_id!)
+  getHandCards() {
+    this.isLoading = true;
+    const urlCards = this.newCardsUrl.replace('<<deck_id>>', this.newDeck.deck_id!)
     fetch(urlCards)
       .then(resp => resp.json())
       .then((data: CardsResponse) => {
-        console.log(data)
-        this.mapCards(data.cards)
+        console.log('CARDS RESPONSE', data);
+        this.remaining = data.remaining
+        this.mapCards(data.cards);
+        setTimeout(() => {
+          this.isLoading = false;
+        }, 500)
       })
   }
 
+
   // Mapeamos las Cartas que nos llegan por el endpoint para poder cambiar su propiedad value por un número //
   mapCards(cards: Card[]) {
-    this.newCards = cards.map(card => {
+    this.handCards = [...this.handCards, ...cards.map(card => {
       if (isNaN(Number(card.value)) === true) {
-        console.log('NaN', card.value)
         card.value = this.transformValues(card.value.toString())
       }
       card.value = Number(card.value)
       return card
-    })
-    console.log(this.newCards)
+    })]
+    console.log('HAND CARDS', this.handCards);
+    this.setWinner();
   }
 
   // Transfomamos los valores que nos llegan de las Cards AS, JACK, QUEEN, KING //
   transformValues(cardValue: string): string {
     switch (cardValue) {
-      case 'AS':
-        return '1'
+      case 'ACE':
+        return '14'
 
       case 'JACK':
         return '11'
@@ -77,14 +92,94 @@ export class AppComponent {
   }
 
 
+  // Validamos quien es el ganador y metemos las cartas al ganador de la mano //
+  setWinner() {
+    const indexPlayer: number = this.handCards.length - 2;
+    const indexCpu: number = this.handCards.length - 1;
+    if (this.handCards[indexPlayer].value > this.handCards[indexCpu].value) {
+      this.player = [...this.player, ...this.handCards]
+      console.log('WINNER PLAYER');
+      this.winner = 'player';
+      this.isDraw = false;
+
+    } else if (this.handCards[indexPlayer].value < this.handCards[indexCpu].value) {
+      this.cpu = [...this.cpu, ...this.handCards]
+      console.log('WINNER CPU');
+      this.winner = 'cpu';
+      this.isDraw = false;
+    } else {
+      console.log('EMPATE');
+      this.isDraw = true;
+
+    }
+    console.log('PLAYER', this.player)
+    console.log('CPU', this.cpu)
+    if (this.remaining === 0) {
+      this.endGame()
+    }
+
+  }
+
+  // Botón para ejecutar la jugada siguiete //
+  nextHand() {
+    this.winner = '';
+    if (!this.isDraw) {
+
+      this.handCards = [];
+    }
+    this.getHandCards();
+    console.log('-----------------------')
+
+  }
+
+  // Botón para inicial el juego desde cero //
+  init() {
+    this.endGameMessage = '';
+    this.handCards = [];
+    this.cpu = [];
+    this.player = [];
+    this.getNewDeck()
+  }
+
+  endGame() {
+    switch (true) {
+      case this.player.length > this.cpu.length:
+        this.endGameMessage = `FELICIDADES PLAYER!!! GANASTE CON ${this.player.length} CARTAS`
+        break;
+      case this.player.length < this.cpu.length:
+        this.endGameMessage = `OOOOOH PLAYER, PERDISTE!!! LA CPU SE HA QUEDADO CON ${this.cpu.length} CARTAS`
+        break;
+      default:
+        this.endGameMessage = 'EMPATARON CARA CULOS'
+        break;
+    }
+    this.openModal()
+  }
+
+  openModal() {
+    this.isShowModal = true;
+  }
 
 
-
-
-
+  closeModal() {
+    this.isShowModal = false;
+    this.endGameMessage = '';
+  }
 
 
 
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
